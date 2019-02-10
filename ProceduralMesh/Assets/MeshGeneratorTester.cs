d3c2efb2bc036a310b8aa3e3815f95891b3cdd89
Unknown <ma.van.dinther@sintlucasedu.nl>
2019-02-10 23:18:26 +0100

    Finished Procedural Worldbuilding
    Added Worldbuilding along with a script for showing it in action

---
+++ ProceduralMesh/Assets/MeshGeneratorTester.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(MeshFilter))]
+public class MeshGeneratorTester : MonoBehaviour
+{
+    Mesh mesh;
+
+    Vector3[] vertices;
+    int[] triangles;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        mesh = new Mesh();
+        GetComponent<MeshFilter>().mesh = mesh;
+
+        CreateShape();
+        UpdateMesh();
+    }
+
+    void CreateShape()
+    {
+        vertices = new Vector3[]
+        {
+            new Vector3(0,0,0),
+            new Vector3(0,0,1),
+            new Vector3(1,0,0),
+            new Vector3(1,0,1)
+        };
+
+        triangles = new int[]
+        {
+            0,1,2,
+            1,3,2
+        };
+    }
+
+    void UpdateMesh()
+    {
+        mesh.Clear();
+
+        mesh.vertices = vertices;
+        mesh.triangles = triangles;
+
+        mesh.RecalculateNormals();
+    }
+}
